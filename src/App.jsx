6e5d6a13887ad6ffc0d4f1f87e3cecc9cf0b5813
Anshumik <96510803+Anshumik@users.noreply.github.com>
2024-01-24 18:36:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './app.scss';
-import { Contact} from './components/contact/contact';
+import { Contact} from './components/contact/Contact';
 import Hero from './components/hero/hero';
 import { Navbar } from './components/navbar/Navbar';
 import { Parallax } from './components/parallax/Parallax';
